1fac61207af1a2fcb2d4682ee19b11d44c0b6782
azh <azhandhj@outlook.com>
2023-05-11 23:50:44 +0800

    alter rule

--- 2048/main.c
+++ 2048/main.c
@@ -16,18 +16,15 @@ static void showTime(){
     time_t costTime=endTime-startTime;
     printf("Time %ld%ld:%ld%ld  ",costTime/(60*10),costTime/60%10,(costTime%60-costTime%10)/10,costTime%10);
 }
-static void addRandomPoint(int count){
+static void addRandomPoint(){
     int pos=-1;
-    for(int i=0;i<count;i++){
-        pos=-1;
-        while(isInvaild(pos)){
-            pos=getRandomNumber(16);
-            if(isFull()){
-                return;
-            }
+    while(pos==-1||isInvaild(pos)){
+        pos=getRandomNumber(16);
+        if(isFull()){
+            return;
         }
-        addPoint(pos,(getRandomNumber(1)+1)*2);
     }
+    addPoint(pos,(getRandomNumber(1)+1)*2);
 }
 int main(){
     printf("**************\n");
@@ -44,7 +41,8 @@ int main(){
         case 1:
         startTime=time(NULL);
         initalGame();
-        addRandomPoint(2);
+        addRandomPoint();
+        addRandomPoint();
         do{
             clearScreen();
             showTime();
@@ -54,30 +52,22 @@ int main(){
             {
             case MOVE_FRONT:
                 moveFront();
-                if(!isFull()){
-                    addRandomPoint(2);
-                }
                 break;
             case MOVE_LEFT:
                 moveLeft();
-                if(!isFull()){
-                    addRandomPoint(2);
-                }
                 break;
             case MOVE_RIGHT:
                 moveRight();
-                if(!isFull()){
-                    addRandomPoint(2);
-                }
                 break;
             case MOVE_BEHIND:
                 moveBehind();
-                if(!isFull()){
-                    addRandomPoint(2);
-                }
                 break;
             default:
+                continue;
                 break;
+            }
+            if(!isFull()){
+                addRandomPoint();
             }
         }while(isWin()==0);
         clearScreen();
